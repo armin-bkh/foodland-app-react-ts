36a3e6be99600d9c9912194a3dcb8d68fe7a102a
armin-bkh <arminbkh0921@yahoo.com>
2022-01-29 14:21:10 +0330

    create FoodsPage component

---
+++ src/Pages/FoodsPage.tsx
@@ -0,0 +1,13 @@
+import Layout from "../Layout/Layout"
+
+const FoodsPage = () => {
+    return (
+        <Layout>
+            <main className="main">
+            FoodsPage
+            </main>
+        </Layout>
+    )
+}
+
+export default FoodsPage;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,8 +1,9 @@
+import FoodsPage from "../Pages/FoodsPage";
 import HomePage from "../Pages/HomePage";
 
 const routes = [
   { path: "/", element: <HomePage /> },
-  // { path: "/foods", element: <HomePage /> },
+  { path: "/foods", element: <FoodsPage /> },
 ];
 
 export default routes;
