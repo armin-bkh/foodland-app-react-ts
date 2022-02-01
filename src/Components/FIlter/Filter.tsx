7809fa08c9307f295caf60ed3c3993ed71e59afa
armin-bkh <arminbkh0921@yahoo.com>
2022-02-01 16:33:38 +0330

    update responsive

--- src/Components/FIlter/Filter.tsx
+++ src/Components/FIlter/Filter.tsx
@@ -2,14 +2,14 @@ import { BiSearchAlt } from "react-icons/bi";
 
 const Filter = () => {
   return (
-    <section>
-      <div className="flex rounded-sm overflow-hidden shadow focus-within:shadow-md">
+    <section className="flex flex-col">
+      <div className="text top-12 flex rounded-l-sm shadow focus-within:shadow-md">
         <input
-          className="px-5 flex-1 outline-none py-2"
+          className="px-5 flex-1 w-4/5 outline-none py-2"
           type="text"
           placeholder="searching for..."
         />
-        <button className="p-3 rounded-sm bg-red-400 text-white">
+        <button className="p-3  rounded-r-sm bg-red-400 text-white">
           <BiSearchAlt />
         </button>
       </div>
