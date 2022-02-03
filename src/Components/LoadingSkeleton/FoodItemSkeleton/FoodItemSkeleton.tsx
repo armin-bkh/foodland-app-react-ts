f57a501a75a3942a024eabf1101b0ac2e1a6af09
armin-bkh <arminbkh0921@yahoo.com>
2022-02-03 15:08:56 +0330

    update FoodItemSkeleton component

--- src/Components/LoadingSkeleton/FoodItemSkeleton/FoodItemSkeleton.tsx
+++ src/Components/LoadingSkeleton/FoodItemSkeleton/FoodItemSkeleton.tsx
@@ -4,7 +4,7 @@ export const FoodItemSkeleton = () => {
   return (
     <SkeletonTheme borderRadius={0} baseColor="#e7e7e7" highlightColor="#c9c9c9">
       <div className="shadow rounded-lg overflow-hidden flex flex-col">
-          <Skeleton width={`100%`} className={`h-40 lg:h-52 xl:h-56`} />
+          <Skeleton width={`100%`} className={`h-56`} />
         <div className="py-6 px-3 flex-1">
           <div>
             <Skeleton borderRadius={5} width={60} height={20} />
