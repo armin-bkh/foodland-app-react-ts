0532635afcd9b6b738993d86a7fe58fa8f9b3478
armin-bkh <arminbkh0921@yahoo.com>
2022-02-03 15:04:28 +0330

    update ui HomePage component

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,28 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import Layout from "../Layout/Layout";
-import HomeBg from "../Assets/SVG/hamburger-animate.svg";
+import HomeBg from "../Assets/Images/Hamburger-bro.png";
 const HomePage = () => {
   const navigate = useNavigate();
   return (
     <Layout>
-      <main className="main">
-        <h1 className="text-5xl lg:text-7xl">Food Land</h1>
-        <section>
-          {/* <img src={HomeBg} alt="bakgroung" /> */}
+      <main className="main grid grid-cols-3 grid-row-3">
+        <h1 className="text-5xl lg:text-7xl col-start-1 col-end-3">
+          Food Land
+        </h1>
+        <section className="col-start-1 col-end-4">
+          <p className="my-16 text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            fugiat consequatur, rerum, amet a animi dolorem excepturi fugit
+            aperiam tempore quibusdam molestiae, eveniet nemo maxime! Beatae
+            laborum eius placeat officiis!
+          </p>
+          <button
+            type="button"
+            onClick={() => navigate("/foods")}
+            className="homePageButton"
+          >
+            Reserve...
+          </button>
         </section>
-        <p className="my-16 text-gray-600">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla fugiat
-          consequatur, rerum, amet a animi dolorem excepturi fugit aperiam
-          tempore quibusdam molestiae, eveniet nemo maxime! Beatae laborum eius
-          placeat officiis!
-        </p>
-        <button
-          type="button"
-          onClick={() => navigate("/foods")}
-          className="homePageButton"
-        >
-          Reserve...
-        </button>
+        <section className="flex col-start-3 col-end-4 row-start-1 items-start lg:items-end">
+          <img className="w-full" src={HomeBg} alt="bakgroung" />
+        </section>
       </main>
     </Layout>
   );
